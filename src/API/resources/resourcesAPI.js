0b7ea0b1701c1b8e52aecafa06b89f1a2f9638aa
Ran Kirlian <rankirlian@gmail.com>
2022-05-02 18:42:53 +0200

    Add Mintlify comments to resourcesAPI

--- src/API/resources/resourcesAPI.js
+++ src/API/resources/resourcesAPI.js
@@ -4,6 +4,11 @@ const employeesAPI = process.env.REACT_APP_BACKEND + '/api/employees';
 
 
 // Lists all employees
+/**
+ * This function will make a GET request to the employeesAPI, and if the response is successful, it
+ * will return the response.
+ * @returns The response object.
+ */
 const getEmployees = async () => {
   try {
     const response = await axios.get(employeesAPI)
@@ -15,6 +20,12 @@ const getEmployees = async () => {
 };
 
 // Lists one specific employee by id
+/**
+ * This function will make a GET request to the employees API and return the response if the status is
+ * 200.
+ * @param id - the id of the employee you want to get
+ * @returns The response object.
+ */
 const getOneEmployee = async (id) => {
   try {
     const response = await axios.get(`${employeesAPI}/${id}`)
@@ -25,6 +36,12 @@ const getOneEmployee = async (id) => {
   }
 };
 
+/**
+ * It takes in an employeeData object, and then it makes a POST request to the employeesAPI endpoint,
+ * and then it returns the response if the response status is 200.
+ * @param employeeData - {
+ * @returns The response object.
+ */
 const postOneEmployee = async (employeeData) => {
   try {
 
@@ -36,6 +53,12 @@ const postOneEmployee = async (employeeData) => {
   }
 };
 
+/**
+ * It takes an id as an argument, and then it makes a delete request to the API, and if the response is
+ * successful, it returns the response.
+ * @param id - the id of the employee you want to delete
+ * @returns The response object.
+ */
 const deleteOneEmployee = async (id) => {
   try {
     const response = await axios.delete(`${employeesAPI}/${id}`)
